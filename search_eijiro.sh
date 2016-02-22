f80d6134c6b84dbbd8bb56436a0d9117addda5c5
AKANEDaigo <daigo@AKANEDaigo-no-MacBook-Pro.local>
2016-02-23 04:52:08 +0900

    コメントの位置を修正

--- search_eijiro.sh
+++ search_eijiro.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash                                                                                                    
+#!/usr/bin/env bash
 
-EIJIRO_TEXT=./EIJI-144-utf8.TXT
+EIJIRO_TEXT=~/Dropbox/eijiro/EIJI-144-utf8.TXT
 # nkfなどでutf8に変換済みの英辞郎のテキストファイル
 # nkf -w EIJI-144.TXT >EIJI-144-utf8.TXT
 
 WORD=$1
 
 grep -iE "^■$WORD" ${EIJIRO_TEXT} |  \
-sed -e 's/^■//'  | \ # 行頭の■を削除
-sed -e 's/｛[^｝]*｝//g' # ふりがなを削除
+sed -e 's/^■//'  | \
+sed -e 's/｛[^｝]*｝//g'
+# 行頭の■を削除
+# ふりがなを削除
